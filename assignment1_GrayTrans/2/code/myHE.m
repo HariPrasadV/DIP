cb16ea8b09b46e886f78ace4c72b0db937bbdd65
vinayak <vinayakdeepu@gmail.com>
2017-08-03 22:01:32 +0530

    Update myHE.m and main script

--- assignment1_GrayTrans/2/code/myHE.m
+++ assignment1_GrayTrans/2/code/myHE.m
@@ -1,37 +1,13 @@
-clear;
-img = imread('../data/TEM.png');
-   
-[counts, x] = imhist(img);
-
-[m, n] = size(img);
-N = m * n;
-
-probs = counts / N;
-cdf = cumsum(probs);
-
-T = floor(255 * cdf);
-eqImg = T(img + 1);
-
-myNumOfColors = 200;
-myColorScale = [[0:1/(myNumOfColors-1):1]' , ... 
-    [0:1/(myNumOfColors-1):1]' , [0:1/(myNumOfColors-1):1]'];
-
-figure();
-
-subplot(2, 1, 1);
-imagesc(eqImg);
-title('');
-colormap (myColorScale);
-% colormap jet;
-daspect ([1 1 1]);
-axis tight;
-colorbar;
-
-subplot(2, 1, 2);
-imagesc(img);
-title('');
-colormap (myColorScale);
-% colormap jet;
-daspect ([1 1 1]);
-axis tight;
-colorbar;
+function out = myHE(img)
+    % img -> image (any dimension)
+    % out -> histogram equalized img
+    
+    [m, n, d] = size(img);
+    N = m * n;
+    for ii = 1:d
+        [counts, ~] = imhist(img(:,:,ii));
+        probs = counts / N;
+        cdf = cumsum(probs);
+        T = floor(255 * cdf);
+        out(:,:,ii) = uint8(T(img(:,:,ii) + 1));
+    end
--- assignment1_GrayTrans/2/code/myMainScript.m
+++ assignment1_GrayTrans/2/code/myMainScript.m
@@ -32,9 +32,9 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-location = '../images/a/barbara.png';
-saveas(gcf, location);
-disp(strcat('saved linearly contrast stretched `barbara.png` as `', location, '`'));
+% location = '../images/a/barbara.png';
+% saveas(gcf, location);
+% disp(strcat('saved linearly contrast stretched `barbara.png` as `', location, '`'));
 
 
 % canyon --------
@@ -61,9 +61,9 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-location = '../images/a/canyon.png';
-saveas(gcf, location);
-disp(strcat('saved linearly contrast stretched `canyon.png` as `', location, '`'));
+% location = '../images/a/canyon.png';
+% saveas(gcf, location);
+% disp(strcat('saved linearly contrast stretched `canyon.png` as `', location, '`'));
 
 % TEM --------
 TEMImg = imread('../data/TEM.png');
@@ -89,10 +89,95 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-location = '../images/a/TEM.png';
-saveas(gcf, location);
-disp(strcat('saved linearly contrast stretched `TEM.png` as `', location, '`'));
+% location = '../images/a/TEM.png';
+% saveas(gcf, location);
+% disp(strcat('saved linearly contrast stretched `TEM.png` as `', location, '`'));
+
 toc;
 
 
 %% Q2.b - HE
+
+% barbara -----------
+barbaraImg = imread('../data/barbara.png');
+heBarbaraImg = myHE(barbaraImg);
+
+tic;
+figure();
+
+subplot(1, 2, 1);
+imagesc(barbaraImg);
+title('original');
+colormap (myColorScale);
+% colormap jet;
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+subplot(1, 2, 2);
+imagesc(heBarbaraImg);
+title('histogram equalized');
+colormap (myColorScale);
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+% location = '../images/b/barbara.png';
+% saveas(gcf, location);
+% disp(strcat('saved histogram equalized `barbara.png` as `', location, '`'));
+
+
+% canyon ---
+canyonImg = imread('../data/canyon.png');
+heCanyonImg = myHE(canyonImg);
+
+figure();
+
+subplot(2, 1, 1);
+imagesc(canyonImg);
+title('original');
+colormap (myColorScale);
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+subplot(2, 1, 2);
+imagesc(heCanyonImg);
+title('histogram equalized');
+colormap (myColorScale);
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+% location = '../images/b/canyon.png';
+% saveas(gcf, location);
+% disp(strcat('saved histogram equalized `canyon.png` as `', location, '`'));
+
+% TEM --------
+TEMImg = imread('../data/TEM.png');
+heTEMImg = myHE(TEMImg);
+
+figure();
+
+subplot(2, 1, 1);
+imagesc(TEMImg);
+title('original');
+colormap (myColorScale);
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+subplot(2, 1, 2);
+imagesc(heTEMImg);
+title('histogram equalized');
+colormap (myColorScale);
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+% location = '../images/b/TEM.png';
+% saveas(gcf, location);
+% disp(strcat('saved histogram equalized `TEM.png` as `', location, '`'));
+
+toc;
+
