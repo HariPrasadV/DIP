176e50f445662c3c63e7e5e01a43e583e3527305
vinayak <vinayakdeepu@gmail.com>
2017-08-06 16:37:46 +0530

    Completed part 2 report

--- assignment1_GrayTrans/2/code/myCLAHE.m
+++ assignment1_GrayTrans/2/code/myCLAHE.m
@@ -1,29 +1,50 @@
-function out = myCLAHE(img, threshold)
-    
+function out = myCLAHE(img, windowSize, threshold)
+      
+    w = floor(windowSize / 2);
+
     [m, n, d] = size(img);
-    N = m * n;
-    for ii = 1:d
-        [counts, ~] = imhist(img(:,:,ii));
+    
+    % eqImg = blockproc(img, [d d], @equalize);
+    % eqImg = nlfilter(img, [d d], @equalize);
+    h = waitbar(0, 'running CLAHE');
+    step = 0;
+    steps = d * m;
+    for kk = 1:d
+        for ii = 1:m
+            for jj = 1:n
+                t = max(1, ii - w + 1);
+                b = min(ii + w, m);
+                l = max(1, jj - w + 1);
+                r = min(jj + w, n);
+                window = img(t:b, l:r, kk);   
+                out(ii, jj, kk) = equalize(window, [ii - t + 1, jj - l + 1]);
+            end
+            step = step + 1;
+            waitbar(step / steps);
+        end
+    end
+    close(h);
+    
+    function out = equalize(block, pos)
+        [bm, bn] = size(block);
+        N = bm * bn;
+        [counts, ~] = imhist(block);
         probs = counts / N;
         
-        % plot(probs)
         sumAboveThreshold = sum(max(0, probs - threshold));
         extraMass = sumAboveThreshold / 256;
         
-        for jj = 1:256
-            if probs(jj) > threshold
-                probs(jj) = threshold;
+        for k = 1:256
+            if probs(k) > threshold
+                probs(k) = threshold;
             end
         end
         
         probs = probs + extraMass;
         
-        % figure();
-        % plot(probs)
-        
         cdf = cumsum(probs);
-        T = 255 * cdf;
-        out(:,:,ii) = uint8(T(img(:,:,ii) + 1));
-    end
+        out = uint8(255 * cdf(block(pos(1), pos(2)) + 1)); 
+    end 
+end
     
     
--- assignment1_GrayTrans/2/code/myMainScript.m
+++ assignment1_GrayTrans/2/code/myMainScript.m
@@ -8,7 +8,6 @@ tic;
 disp('linear contrast stretching..');
 
 % barbara -----------
-
 barbaraImg = imread('../data/barbara.png');
 lcsBarbaraImg = myLinearContrastStretching(barbaraImg);
 
@@ -32,10 +31,9 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/a/barbara.png';
-% saveas(gcf, location);
-% disp(strcat('saved linearly contrast stretched `barbara.png` as `', location, '`'));
-
+location = '../images/a/barbara.png';
+saveas(gcf, location);
+disp(strcat('saved linearly contrast stretched `barbara.png` as `', location, '`'));
 
 % canyon --------
 canyonImg = imread('../data/canyon.png');
@@ -61,9 +59,9 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/a/canyon.png';
-% saveas(gcf, location);
-% disp(strcat('saved linearly contrast stretched `canyon.png` as `', location, '`'));
+location = '../images/a/canyon.png';
+saveas(gcf, location);
+disp(strcat('saved linearly contrast stretched `canyon.png` as `', location, '`'));
 
 % TEM --------
 TEMImg = imread('../data/TEM.png');
@@ -89,20 +87,20 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/a/TEM.png';
-% saveas(gcf, location);
-% disp(strcat('saved linearly contrast stretched `TEM.png` as `', location, '`'));
+location = '../images/a/TEM.png';
+saveas(gcf, location);
+disp(strcat('saved linearly contrast stretched `TEM.png` as `', location, '`'));
 
 toc;
 
 
 %% Q2.b - HE
+tic;
 
 % barbara -----------
 barbaraImg = imread('../data/barbara.png');
 heBarbaraImg = myHE(barbaraImg);
 
-tic;
 figure();
 
 subplot(1, 2, 1);
@@ -121,10 +119,9 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/b/barbara.png';
-% saveas(gcf, location);
-% disp(strcat('saved histogram equalized `barbara.png` as `', location, '`'));
-
+location = '../images/b/barbara.png';
+saveas(gcf, location);
+disp(strcat('saved histogram equalized `barbara.png` as `', location, '`'));
 
 % canyon ---
 canyonImg = imread('../data/canyon.png');
@@ -148,9 +145,9 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/b/canyon.png';
-% saveas(gcf, location);
-% disp(strcat('saved histogram equalized `canyon.png` as `', location, '`'));
+location = '../images/b/canyon.png';
+saveas(gcf, location);
+disp(strcat('saved histogram equalized `canyon.png` as `', location, '`'));
 
 % TEM --------
 TEMImg = imread('../data/TEM.png');
@@ -174,15 +171,15 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/b/TEM.png';
-% saveas(gcf, location);
-% disp(strcat('saved histogram equalized `TEM.png` as `', location, '`'));
+location = '../images/b/TEM.png';
+saveas(gcf, location);
+disp(strcat('saved histogram equalized `TEM.png` as `', location, '`'));
 
 toc;
 
 %% Q2.c AHE
 
-% TEM -------------------
+% TEM AHE -------------------
 tic;
 
 TEMImg = imread('../data/TEM.png');
@@ -206,11 +203,12 @@ daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/c/TEM.png';
-% saveas(gcf, location);
-% disp(strcat('saved adaptive histogram equalized `TEM.png` as `', location, '`'));
+location = '../images/c/TEM.png';
+saveas(gcf, location);
+disp(strcat('saved adaptive histogram equalized `TEM.png` as `', location, '`'));
 
-% canyon ---------
+%% canyon AHE ---------
+
 canyonImg = imread('../data/canyon.png');
 aheCanyonImg = myAHE(canyonImg, 100);
 
@@ -226,21 +224,22 @@ colorbar;
 
 subplot(2, 1, 2);
 imagesc(aheCanyonImg);
-title('adaptive histogram equalized, w = 100');
+title('AHE, w = 100');
 colormap (myColorScale);
 daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/c/canyon.png';
-% saveas(gcf, location);
-% disp(strcat('saved adaptive histogram equalized `canyon.png` as `', location, '`'));
+location = '../images/c/canyon.png';
+saveas(gcf, location);
+disp(strcat('saved adaptive histogram equalized `canyon.png` as `', location, '`'));
 
-%% barbara --------------------
+% barbara AHE --------------------
+tic;
+
 barbaraImg = imread('../data/barbara.png');
 aheBarbaraImg = myAHE(barbaraImg, 100);
 
-tic;
 figure();
 
 subplot(1, 2, 1);
@@ -253,27 +252,26 @@ colorbar;
 
 subplot(1, 2, 2);
 imagesc(aheBarbaraImg);
-title('adaptive histogram equalized, w = 100');
+title('AHE, w = 100');
 colormap (myColorScale);
 daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/c/barbara.png';
-% saveas(gcf, location);
-% disp(strcat('saved adaptive histogram equalized `barbara.png` as `', location, '`'));
+location = '../images/c/barbara.png';
+saveas(gcf, location);
+disp(strcat('saved adaptive histogram equalized `barbara.png` as `', location, '`'));
 
 toc;
 
 
 %% Q2.d - CLAHE
-
-% TEM
 tic;
 
+% TEM ---------------
+
 TEMImg = imread('../data/TEM.png');
-% claheTEMImg = myCLAHE(TEMImg, 0.05);
-claheTEMImg = myCLAHE(TEMImg, 0.1);
+claheTEMImg = myCLAHE(TEMImg, 100, 0.005);
 
 figure();
 
@@ -287,14 +285,68 @@ colorbar;
 
 subplot(2, 1, 2);
 imagesc(claheTEMImg);
-title('CLAHE');
+title('CLAHE, w = 100, threshold = 0.005');
 colormap (myColorScale);
 daspect ([1 1 1]);
 axis tight;
 colorbar;
 
-% location = '../images/d/TEM.png';
-% saveas(gcf, location);
-% disp(strcat('saved CALHE `TEM.png` as `', location, '`'));
+location = '../images/d/TEM-0.005.png';
+saveas(gcf, location);
+disp(strcat('saved CALHE `TEM.png` as `', location, '`'));
+
+
+% barbara -------
+barbaraImg = imread('../data/barbara.png');
+claheBarbaraImg = myCLAHE(barbaraImg, 100, 0.005);
+
+figure();
+
+subplot(1, 2, 1);
+imagesc(barbaraImg);
+title('original');
+colormap (myColorScale);
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+subplot(1, 2, 2);
+imagesc(claheBarbaraImg);
+title('CLAHE, w = 100, threshold = 0.005');
+colormap (myColorScale);
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+location = '../images/d/barbara.png';
+saveas(gcf, location);
+disp(strcat('saved CLAHE `barbara.png` as `', location, '`'));
+
+% canyon ---------
+
+canyonImg = imread('../data/canyon.png');
+claheCanyonImg = myCLAHE(canyonImg, 200, 0.05);
+
+figure();
+
+subplot(2, 1, 1);
+imagesc(canyonImg);
+title('original');
+colormap (myColorScale);
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+subplot(2, 1, 2);
+imagesc(claheCanyonImg);
+title('CLAHE, w = 200, threshold = 0.05');
+colormap (myColorScale);
+daspect ([1 1 1]);
+axis tight;
+colorbar;
+
+location = '../images/d/canyon.png';
+saveas(gcf, location);
+disp(strcat('saved CLAHE `canyon.png` as `', location, '`'));
 
 toc;
