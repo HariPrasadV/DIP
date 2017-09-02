47144650930ad92059b6e90357e5ec5462c66e47
vinayak <vinayakdeepu@gmail.com>
2017-09-02 16:31:14 +0530

    Add sample runs of mean shift segmentation

--- assignment3_Segmentation/2/code/myMainScript.m
+++ assignment3_Segmentation/2/code/myMainScript.m
@@ -1,5 +1,8 @@
 %% MyMainScript
 
 tic;
-myMeanShiftSegmentation(16, 16, 200, 10);
+% myMeanShiftSegmentation(16, 16, 200, 10);
+% myMeanShiftSegmentation(48, 16, 200, 5);
+% myMeanShiftSegmentation(2, 2, 200, 10);
+myMeanShiftSegmentation(16, 24, 200, 10);
 toc;
