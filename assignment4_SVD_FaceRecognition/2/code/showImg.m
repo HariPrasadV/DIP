7ee5cc4314d5896b22c9c4e7743fc33b2679b1c2
vinayak <vinayakdeepu@gmail.com>
2017-10-12 01:16:44 +0530

    Finished Q3

---
+++ assignment4_SVD_FaceRecognition/2/code/showImg.m
@@ -0,0 +1,16 @@
+function [] = showImg(I, title_text)
+    myNumOfColors = 200;
+    myColorScale = [[0:1/(myNumOfColors-1):1]', ... 
+                [0:1/(myNumOfColors-1):1]', ...
+                [0:1/(myNumOfColors-1):1]'];
+
+    iptsetpref('ImshowAxesVisible','on');
+	figure('units','normalized','outerposition',[0 0 1 1])
+    imagesc(I);
+    colormap (myColorScale);
+    daspect ([1 1 1]);
+    axis tight;
+    title(title_text)
+    
+    set(findall(gcf,'-property','FontSize'),'FontSize',20)
+end
